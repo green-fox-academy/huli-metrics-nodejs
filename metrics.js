f9e06a0f62c932f2a1aa1a574b89de80083fc08e
fehersanyi <papahadacow@gmail.com>
2018-05-29 14:59:47 +0200

    tested it, it is working now

--- metrics.js
+++ metrics.js
@@ -1,19 +1,20 @@
 'use strict';
+const fetch = require('node-fetch');
 
 module.exports = {
-
   send(applicationName, serviceName) {
-    return fetch('internalmetrics.com/event', {
+    return fetch('http://localhost:3000', {
       body: JSON.stringify({
         time: Date.now(),
         level: serviceName,
-        service_name: applicationName
-    }),
+        service_name: applicationName,
+      }),
       headers: {
         'content-type': 'application/json'
       },
       method: 'POST',
     })
-    .then(response => response.json());
-  }
+    .then(response => (response.json()))
+    .then(data => (data));
+  },
 };
